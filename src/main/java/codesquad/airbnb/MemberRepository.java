3ee2605d32d3909742416bb79d565f3fb2fee80d
jinan159 <wlsdhks0423@naver.com>
2022-05-26 19:50:19 +0900

    JPA 테스트 추가

---
+++ src/main/java/codesquad/airbnb/MemberRepository.java
@@ -0,0 +1,6 @@
+package codesquad.airbnb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
--- src/test/java/codesquad/airbnb/AirbnbTest.java
+++ src/test/java/codesquad/airbnb/AirbnbTest.java
@@ -2,10 +2,17 @@ package codesquad.airbnb;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest
 class AirbnbTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("Test case 1) 1 == 1")
@@ -17,5 +24,19 @@ class AirbnbTest {
     @DisplayName("Test case 2) 2 > 1")
     void test2() {
         assertThat(2).isGreaterThan(1);
+    }
+
+    @Test
+    @DisplayName("Test case 3) JPA Test")
+    void test3() {
+        // given
+        Member member = new Member();
+        member.setName("jwkim");
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        assertThat(savedMember.getName()).isEqualTo(member.getName());
     }
 }

9757f43e8eff3b84069554bb7d2ea47b7fb3fe53
jinan159 <wlsdhks0423@naver.com>
2022-05-26 17:59:34 +0900

    Add Test cases

---
+++ src/test/java/codesquad/airbnb/AirbnbTest.java
@@ -0,0 +1,21 @@
+package codesquad.airbnb;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AirbnbTest {
+
+    @Test
+    @DisplayName("Test case 1) 1 == 1")
+    void test1() {
+        assertThat(1).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Test case 2) 1 > 1")
+    void test2() {
+        assertThat(1).isGreaterThan(1);
+    }
+}

110b59e01d5898b18dad1a0e76f5708c85b4da60
jinan159 <wlsdhks0423@naver.com>
2022-05-26 15:17:42 +0900

    first commit

---
+++ src/main/java/codesquad/airbnb/DefaultController.java
@@ -0,0 +1,13 @@
+package codesquad.airbnb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
